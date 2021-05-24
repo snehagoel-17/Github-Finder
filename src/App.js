ae730660daf08f56a9523addbb499ac3b8a04b51
snehagoel-17 <67822014+snehagoel-17@users.noreply.github.com>
2021-05-24 15:32:41 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React,{ Fragment, useState} from'react';
+import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Navbar from './components/layout/Navbar'
+import Home from './components/pages/Home'
+// import NotFound from './components/pages/NotFoundgit'
+import User from './components/users/User'
+
+import Alert from './components/layout/Alert';
+import About from './components/pages/About'
+import GithubState from './context/github/GithubState'
+import AlertState from './context/alert/AlertState'
+
+const App=()=> {
+
+
+
+//  async componentDidMount(){
+//    this.setState({loading:true});
+
+//   const res=await axios.get(`http://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+//    this.setState({users:res.data,loading:false});
+
+
+
+ 
+  return (
+    <GithubState>
+      <AlertState>
+    <Router>
+    <div className="App"
+    >
+      
+       <Navbar/>
+       <div className='container'>
+         <Alert />
+         <Switch>
+           <Route exact path='/' component={Home}/>
+           <Route exact path='/about' component={About}/>
+           <Route exact path='/user/:login' component={User} />
+           {/* <Route component={NotFound} /> */}
+         </Switch>
+         
+       </div>
+      
+    </div>
+    </Router>
+    </AlertState>
+    </GithubState>
+  );
+}
+
+export default App;
